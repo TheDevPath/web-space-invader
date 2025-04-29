3a5b22144e30dc3bcf122e6534ba5c06bfdbfabc
Bryan Sharpley <motosharpley@gmail.com>
2025-04-29 10:31:08 -0700

    use canvas to draw our player ship

--- src/main.js
+++ src/main.js
@@ -13,12 +13,39 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+let ship;
+
 function preload() {
   // Load assets here
 }
 
 function create() {
-  // Initialize game objects here
+  // Create a graphics object
+  const graphics = this.add.graphics();
+
+  // Set the fill color to white
+  graphics.fillStyle(0xffffff, 1);
+
+  // Draw the ship's body (a rectangle)
+  const shipWidth = 50;
+  const shipHeight = 20;
+  const cannonHeight = 10;
+  const canvasWidth = this.sys.game.config.width;
+  const canvasHeight = this.sys.game.config.height;
+
+  // Center the ship horizontally on the canvas
+  const shipX = canvasWidth / 2 - shipWidth / 2;
+  const shipY = canvasHeight - shipHeight - 10; // 10px above the bottom
+
+  graphics.fillRect(shipX, shipY, shipWidth, shipHeight);
+
+  // Draw the ship's cannon (a smaller rectangle on top)
+  const cannonX = shipX + shipWidth / 2 - 5;
+  const cannonY = shipY - cannonHeight;
+  graphics.fillRect(cannonX, cannonY, 10, cannonHeight);
+
+  // Store the ship's position for movement
+  ship = { x: shipX, y: shipY, width: shipWidth, height: shipHeight };
 }
 
 function update() {
